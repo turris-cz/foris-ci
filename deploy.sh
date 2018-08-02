48781417fcca31465b7f6efda51acd49d6bd28cf
Stepan Henek <stepan.henek@nic.cz>
2018-08-02 12:22:08 +0200

    build images for both python2 and python3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 docker login registry.labs.nic.cz
-docker build -t registry.labs.nic.cz/turris/foris-ci .
-docker push registry.labs.nic.cz/turris/foris-ci
+
+cd python2
+docker build -t registry.labs.nic.cz/turris/foris-ci/python2 .
+docker push registry.labs.nic.cz/turris/foris-ci/python2
+
+cd ../python3
+docker build -t registry.labs.nic.cz/turris/foris-ci/python3 .
+docker push registry.labs.nic.cz/turris/foris-ci/python3
