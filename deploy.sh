6f1ca7101515bf001ce5bf677de5ea59763cc0f8
Stepan Henek <stepan.henek@nic.cz>
2020-12-03 09:56:17 +0100

    remove building of python2 image

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,5 @@
 
 docker login registry.labs.nic.cz
 
-cd python2
-docker build -t registry.labs.nic.cz/turris/foris-ci/python2 .
-docker push registry.labs.nic.cz/turris/foris-ci/python2
-
-cd ../python3
 docker build -t registry.labs.nic.cz/turris/foris-ci/python3 .
 docker push registry.labs.nic.cz/turris/foris-ci/python3
